7b48081cf3ac42839e49f4b0358ab99d925ce475
José David Rico Días <davidricodias.dev@gmail.com>
2022-05-17 14:43:19 +0200

    Added input functionality

--- main.js
+++ main.js
@@ -227,21 +227,22 @@ class Mars {
 }
 
 
-// Example
-mars = new Mars(5, 3);
-mars.addRobot(1, 1, 'E', 'RFRFRFRF');
-mars.addRobot(3, 2, 'N', 'FRRFLLFFRRFLL');
-mars.addRobot(0, 3, 'W', 'LLFFFRFLFL');
-mars.addRobot(3, 2, 'N', 'FFFLF');
-mars.executeRobots();
-
-// Read input
+// Load input
+const fs = require('fs');
+const data = fs.readFileSync('./input.txt', {encoding:'utf8', flag:'r'}).split('\n');
 
 // Create map
+let max_x = data[0].split(" ")[0];
+let max_y = data[0].split(" ")[1];
+mars = new Mars(max_x, max_y);
 
 // Add robots
-
+for(var i=1; i<data.length; i+=2){
+	initial_position = data[i].split(" ");
+	instructions = data[i+1];
+	mars.addRobot(initial_position[0], initial_position[1], initial_position[2], instructions);
+}
 // Execute instructions
-
+mars.executeRobots();
 // Write ouput
 console.log(mars.toString());
